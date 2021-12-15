adb65ab3be1608a3b0364fc9ac899f7550590156
Sblbl <marta.fioravanti.1996@gmail.com>
2021-12-15 10:43:54 +0100

    test on blog pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,26 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import { container } from './layout.module.scss'
 
 const Layout = ({pageTitle, children}) => {
+	const data = useStaticQuery( graphql`
+			query {
+				site {
+					siteMetadata {
+						title
+					}
+				}
+			}
+		`)
 	return (
 		<div>
-			<title>pageTitle</title>
+			<title> { pageTitle } | { data.site.siteMetadata.title } </title>
+			<header> { data.site.siteMetadata.title } </header>
 			<nav>
 				<ul>
 					<li><Link to='/'>home</Link></li>
 					<li><Link to='/stuff'>stuff</Link></li>
+					<li><Link to='/blog'>blog</Link></li>
 				</ul>
 			</nav>
 			<main className={container}>
